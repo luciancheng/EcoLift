249c67d26666485c8475d655898d0f7aa8126ec7
Lucian Cheng <luciancheng3@gmail.com>
2026-03-31 10:16:13 -0400

    ok

--- pi/servo.py
+++ pi/servo.py
@@ -34,7 +34,7 @@ class ServoController:
     SERVO2_DOWN = DEFAULT_POS +  560   # 950
 
     POLL_INTERVAL = 0.05  # 20 Hz
-    RESET_DOWN_DURATION = 3.0
+    RESET_DOWN_DURATION = 2.75
 
     def __init__(self, device: str):
         self.device = device
